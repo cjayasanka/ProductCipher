1aba07e61165e65599bb79e885d012b07b3a70db
Chathura Jayasanka <cjayasanka93@gmail.com>
2018-02-24 19:04:19 +0530

    Basic file read

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
-public class Main {
+import java.io.*;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main {
+    public static void main(String [] args) {
+
+        String fileName = "plain.txt";
+
+        String line;
+
+        try {
+            FileReader fileReader =
+                    new FileReader(fileName);
+
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                char[] ch = line.toCharArray();
+                System.out.println(ch.length);
+                System.out.println(line);
+            }
+
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+        }
     }
 }
